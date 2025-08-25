6557379f02943241e1d971359482f49328abd22d
dittegilsfeldt <dittegilsfeldt@gmail.com>
2025-08-25 08:53:33 +0200

    delete project

--- Customer_Lifetime_Value_Prediction_for_a_Subscription_Service/model_training.py
+++
@@ -1,9 +0,0 @@
-'''
-Purpose: Contains code for training models and evaluating them.
-
-Content:
-Model Definition: Define regression models that can be used for CLV prediction.
-Training Functions: Functions to train each model, including splitting the data into training and testing sets.
-Evaluation Functions: Functions to calculate and return model performance metrics.
-Hyperparameter Tuning: (Optional) Code for tuning hyperparameters if using more complex models (like GridSearch or RandomizedSearch).
-'''
